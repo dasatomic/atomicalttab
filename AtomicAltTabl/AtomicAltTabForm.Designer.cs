01e2533234f0697363a40d283220b1f0e4c093d1
Aleksandar Tomic <AleksandarTomic88@gmail.com>
2012-12-10 18:32:12 +0100

    Messing with forms

--- AtomicAltTabl/AtomicAltTabForm.Designer.cs
+++ AtomicAltTabl/AtomicAltTabForm.Designer.cs
@@ -30,38 +30,37 @@ namespace AtomicAltTabl
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this._textboxProcessInput = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // _textboxProcessInput
-            // 
-            this._textboxProcessInput.BackColor = System.Drawing.SystemColors.MenuBar;
-            this._textboxProcessInput.Location = new System.Drawing.Point(12, 12);
-            this._textboxProcessInput.Name = "_textboxProcessInput";
-            this._textboxProcessInput.Size = new System.Drawing.Size(481, 20);
-            this._textboxProcessInput.TabIndex = 0;
-            //this._textboxProcessInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyPressed);
-            this._textboxProcessInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextboxProcessInputTextChanged);
-		    this._textboxProcessInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HandleNewTypedCharacter);
+			this._textboxProcessInput = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// _textboxProcessInput
+			// 
+			this._textboxProcessInput.BackColor = System.Drawing.SystemColors.MenuBar;
+			this._textboxProcessInput.Location = new System.Drawing.Point(23, 12);
+			this._textboxProcessInput.Name = "_textboxProcessInput";
+			this._textboxProcessInput.Size = new System.Drawing.Size(481, 20);
+			this._textboxProcessInput.TabIndex = 0;
+			this._textboxProcessInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextboxProcessInputTextChanged);
+			this._textboxProcessInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.HandleNewTypedCharacter);
+			// 
+			// AtomicAltTabForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.ClientSize = new System.Drawing.Size(503, 50);
+			this.Controls.Add(this._textboxProcessInput);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.MaximizeBox = false;
+			this.Name = "AtomicAltTabForm";
+			this.Opacity = 0.8D;
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "AtomicAltTab";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
-            // 
-            // AtomicAltTabForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(503, 50);
-            this.Controls.Add(this._textboxProcessInput);
-            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximizeBox = false;
-            this.Name = "AtomicAltTabForm";
-            this.Opacity = 0.8D;
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Text = "AtomicAltTab";
-            this.ResumeLayout(false);
-            this.PerformLayout();
 		}
 
 		#endregion
